3ec191f0123d566ab41d1c66c99b42bba8573bf5
Barath Kannan <Barathwaj.Kannan@gmail.com>
2019-09-24 03:11:44 +1000

    blah

--- flite/include/flite/args.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "flite/features.hpp"
-
-cst_val* cst_args(
-    char** argv,
-    int argc,
-    const char* description,
-    cst_features* args);
--- flite/include/flite/hrg.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "flite/features.hpp"
-#include "flite/file.hpp"
-#include "flite/item.hpp"
-#include "flite/relation.hpp"
-#include "flite/utterance.hpp"
-#include "flite/val.hpp"
